af88351ea94a2991d718f80e6970761951059e41
kidlokostik <kidlokostic12@gmail.com>
2024-06-12 19:41:57 +0300

    empty project

---
+++ PizzApp/src/main/java/com/example/pizzapp/PizzAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.pizzapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PizzAppApplication.class, args);
+    }
+
+}
