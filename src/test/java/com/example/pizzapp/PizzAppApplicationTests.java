2ff46a2634d076d93f41286766e04cdad73c1ec0
kidlokostik <91086868+kidlokostik@users.noreply.github.com>
2024-06-13 13:48:29 +0300

    Add files via upload

---
+++ src/test/java/com/example/pizzapp/PizzAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pizzapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
