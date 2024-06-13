faf608b5d80f95f7f1e5c1fa63a2689309fe43ed
kidlokostik <91086868+kidlokostik@users.noreply.github.com>
2024-06-13 14:40:22 +0300

    Add files via upload

--- src/main/java/com/example/pizzapp/PizzAppApplication.java
+++ src/main/java/com/example/pizzapp/PizzAppApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PizzAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PizzAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PizzAppApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/pizzapp/PizzAppApplicationTests.java
+++ src/test/java/com/example/pizzapp/PizzAppApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PizzAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
