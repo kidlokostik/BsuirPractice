a2ea337c34f2caab17c693c9f3d672d70e38fdc4
AniKi9 <romashimanchik04@gmail.com>
2024-06-14 13:25:18 +0300

    классы CategoriesDTO и ProductsDTO

---
+++ src/main/java/com/example/dto/ProductsDTO.java
@@ -0,0 +1,9 @@
+package com.example.dto;
+
+import lombok.Data;
+
+@Data
+public class ProductsDTO {
+    Long product_id;
+    String product_name;
+}
--- src/main/java/com/example/dto/UsersDTO.java
+++ src/main/java/com/example/dto/UsersDTO.java
@@ -1,5 +1,8 @@
 package com.example.dto;
 
+import lombok.Data;
+
+@Data
 public class UsersDTO {
     Long user_id;
     String phone;
