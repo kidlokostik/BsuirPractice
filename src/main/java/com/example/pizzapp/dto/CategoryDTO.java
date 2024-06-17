1cf53d73d536cb38c7ac4da8cd54761641db9e04
Boris2904 <95974028+Boris2904@users.noreply.github.com>
2024-06-17 20:55:26 +0300

    Добавил валидацию в 2 класса

--- src/main/java/com/example/pizzapp/dto/CategoryDTO.java
+++ src/main/java/com/example/pizzapp/dto/CategoryDTO.java
@@ -2,13 +2,14 @@ package com.example.pizzapp.dto;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.*;
 
 @Data
 @NoArgsConstructor
 public class CategoryDTO {
-
+    @NotNull(message = "Значение не может быть равно 0")
     private Long id;
-
+    @NotBlank(message = "Введите имя пользователя")
     private String name;
 
 }
