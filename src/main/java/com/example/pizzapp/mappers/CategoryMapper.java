e12cd8eb8cc1cf805500453cfb1559fe57fae47e
AniKi9 <romashimanchik04@gmail.com>
2024-06-17 21:42:55 +0300

    CategoryMapper edited

--- src/main/java/com/example/pizzapp/mappers/CategoryMapper.java
+++ src/main/java/com/example/pizzapp/mappers/CategoryMapper.java
@@ -1,18 +1,16 @@
 package com.example.pizzapp.mappers;
 
-import com.example.pizzapp.dto.UserDTO;
-import com.example.pizzapp.models.User;
+import com.example.pizzapp.dto.CategoryDTO;
+import com.example.pizzapp.models.Category;
 import org.mapstruct.Mapper;
 
 import java.util.List;
-import com.example.pizzapp.models.Order;
-import org.mapstruct.Mapper;
 
 
 @Mapper(componentModel = "spring")
 public interface CategoryMapper {
     CategoryDTO toDTO(Category category);
-    List<CategoryDTO> toDTO(List<Category> categorys);
+    List<CategoryDTO> toDTO(List<Category> categories);
 
-    User toEntity(CategoryDTO categoryDTO);
+    Category toEntity(CategoryDTO categoryDTO);
 }
