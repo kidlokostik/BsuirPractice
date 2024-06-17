6f0d9b60f4751d02df2fee72d0fd5174f2f5c934
AniKi9 <romashimanchik04@gmail.com>
2024-06-17 20:45:25 +0300

    валидация полей UserDTO

--- src/main/java/com/example/pizzapp/dto/UserDTO.java
+++ src/main/java/com/example/pizzapp/dto/UserDTO.java
@@ -2,18 +2,27 @@ package com.example.pizzapp.dto;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.*;
 
 @Data
 @NoArgsConstructor
 public class UserDTO {
 
+    @NotNull(message = "Значение не может быть равно 0")
     private Long id;
 
+    @NotBlank(message = "Введите номер телефона")
+    @Pattern(regexp = "\\+?[0-9]{7,15}", message = "Телефон должен быть действительным номером")
     private String phone;
 
-    private String user_name;
+    @NotBlank(message = "Введите имя пользователя")
+    private String name;
 
+    @NotBlank(message = "Введите пароль")
+    @Size(min = 6, message = "Пароль должен содержать не менее 6 символов")
     private String password;
 
+    @NotBlank(message = "Введите подтверждение пароля")
+    @Size(min = 6, message = "Пароль должен содержать не менее 6 символов")
     private String confirm_password;
 }
