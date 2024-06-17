60793c54e8ad6158944caa6a35217365f4b315af
Boris2904 <95974028+Boris2904@users.noreply.github.com>
2024-06-17 21:26:18 +0300

    маппер category

---
+++ src/main/java/com/example/pizzapp/mappers/CategoryMapper.java
@@ -0,0 +1,18 @@
+package com.example.pizzapp.mappers;
+
+import com.example.pizzapp.dto.UserDTO;
+import com.example.pizzapp.models.User;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+import com.example.pizzapp.models.Order;
+import org.mapstruct.Mapper;
+
+
+@Mapper(componentModel = "spring")
+public interface CategoryMapper {
+    CategoryDTO toDTO(Category category);
+    List<CategoryDTO> toDTO(List<Category> categorys);
+
+    User toEntity(CategoryDTO categorysDTO);
+}
