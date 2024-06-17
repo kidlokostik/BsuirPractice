63645b1ff4a3fe06885c6881bc96a8cc0b8dd00a
AniKi9 <romashimanchik04@gmail.com>
2024-06-17 22:45:46 +0300

    @Mapping

--- src/main/java/com/example/pizzapp/mappers/ProductMapper.java
+++ src/main/java/com/example/pizzapp/mappers/ProductMapper.java
@@ -1,14 +1,19 @@
 package com.example.pizzapp.mappers;
 
+import com.example.pizzapp.dto.CategoryDTO;
 import com.example.pizzapp.dto.ProductDTO;
+import com.example.pizzapp.models.Category;
 import com.example.pizzapp.models.Product;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
+    @Mapping(source = "product.category.id", target = "category")
     ProductDTO toDTO(Product product);
+
     List<ProductDTO> toDTO(List<Product> products);
 
     Product toEntity(ProductDTO productDTO);
