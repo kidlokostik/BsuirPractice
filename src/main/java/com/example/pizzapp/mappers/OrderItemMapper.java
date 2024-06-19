4eeb34b5785911954ab8ace6f4358b572c72bddc
AniKi9 <romashimanchik04@gmail.com>
2024-06-19 17:06:13 +0300

    OrderItemMapper fix

--- src/main/java/com/example/pizzapp/mappers/OrderItemMapper.java
+++ src/main/java/com/example/pizzapp/mappers/OrderItemMapper.java
@@ -1,11 +1,8 @@
 package com.example.pizzapp.mappers;
 
-import com.example.pizzapp.dto.OrderItemDto;
 import com.example.pizzapp.dto.request.create.OrderItemCreateRequest;
 import com.example.pizzapp.dto.request.update.OrderItemUpdateRequest;
-import com.example.pizzapp.dto.request.update.OrderUpdateRequest;
 import com.example.pizzapp.dto.response.OrderItemResponse;
-import com.example.pizzapp.models.Order;
 import com.example.pizzapp.models.OrderItem;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -23,21 +20,26 @@ public interface OrderItemMapper {
     @Mapping(target = "product", ignore = true)
     @Mapping(target = "order", ignore = true)
     OrderItem createRequestToEntity(OrderItemCreateRequest orderItemCreateRequest);
+    List<OrderItem> createRequestToEntity(List<OrderItemCreateRequest> orderItemCreateRequests);
 
     @Mapping(source = "orderItem.order.id", target = "orderId")
     @Mapping(source = "orderItem.product.id", target = "productId")
     OrderItemUpdateRequest toUpdateRequest(OrderItem orderItem);
-    List<OrderItem> createRequestToEntity(List<OrderItemCreateRequest> orderItemCreateRequests);
+    List<OrderItemUpdateRequest> toUpdateRequest(List<OrderItem> orderItems);
+
     @Mapping(target = "product", ignore = true)
     @Mapping(target = "order", ignore = true)
     OrderItem updateRequestToEntity(OrderItemUpdateRequest orderItemUpdateRequest);
+    List<OrderItem> updateRequestToEntity(List<OrderItemUpdateRequest> orderItemUpdateRequests);
 
     @Mapping(source = "orderItem.order.id", target = "orderId")
     @Mapping(source = "orderItem.product.id", target = "productId")
     OrderItemResponse toResponse(OrderItem orderItem);
+    List<OrderItemResponse> toResponse(List<OrderItem> orderItems);
 
     @Mapping(target = "product", ignore = true)
     @Mapping(target = "order", ignore = true)
     OrderItem responseToEntity(OrderItemResponse orderItemResponse);
+    List<OrderItem> responseToEntity(List<OrderItemResponse> orderItemResponses);
 }
 
