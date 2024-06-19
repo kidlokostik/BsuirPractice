525937e0ebb45217309cd899453e491703360406
Reknudt <pochta.navsegda@mail.ru>
2024-06-19 15:47:30 +0300

    order service

--- src/main/java/com/example/pizzapp/services/impl/OrderServiceImpl.java
+++ src/main/java/com/example/pizzapp/services/impl/OrderServiceImpl.java
@@ -2,8 +2,11 @@ package com.example.pizzapp.services.impl;
 
 import com.example.pizzapp.dto.OrderDto;
 import com.example.pizzapp.dto.request.create.OrderCreateRequest;
+import com.example.pizzapp.dto.request.update.OrderUpdateRequest;
 import com.example.pizzapp.dto.response.OrderResponse;
+import com.example.pizzapp.exception.ResourceNotFoundException;
 import com.example.pizzapp.mappers.OrderMapper;
+import com.example.pizzapp.models.Category;
 import com.example.pizzapp.models.Order;
 import com.example.pizzapp.repositories.OrderRepository;
 import com.example.pizzapp.services.OrderService;
@@ -17,18 +20,26 @@ import java.util.List;
 @RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
+    private final OrderRepository orderRepository;
+    private final OrderMapper orderMapper;
 
-    private OrderRepository orderRepository;
-    private OrderMapper orderMapper;
     @Override
     public OrderResponse createOrder(OrderCreateRequest createOrderRequest) {
-        Order order = orderMapper.toEntity(createOrderRequest);
-        return orderMapper.toDto(orderRepository.save(order));
+        Order order = orderMapper.createRequestToEntity(createOrderRequest);
+        return orderMapper.toResponse(orderRepository.save(order));
     }
 
     @Override
-    public OrderResponse updateOrder(Order order) {
-        return orderRepository.updateOrder(order);
+    public OrderResponse updateOrder(Long id, OrderUpdateRequest updateOrderRequest) {
+        Order newestOrder = findOrderByOrThrow(id);
+        orderMapper.toUpdateRequest(newestOrder);
+
+        Order updateOrder = orderRepository.save(newestOrder);
+        return orderMapper.toResponse(updateOrder);
+    }
+
+    private Order findOrderByOrThrow(Long id) {
+        return orderRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(""));
     }
 
     @Override
@@ -38,13 +49,14 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public OrderResponse getOrderById(Long id) {
-        return orderRepository.getById(id);
+        Order order = findOrderByOrThrow(id);
+        return orderMapper.toResponse(order);
     }
 
     @Override
     public List<OrderResponse> getAllOrders() {
         return orderRepository.findAll().stream()
-                .map(orderMapper::toDto)
+                .map(orderMapper::toResponse)
                 .toList();
     }
 }
