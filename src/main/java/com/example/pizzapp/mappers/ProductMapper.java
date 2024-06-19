205e38b4335de4d9be85b212c156a2f093bd8dc8
AniKi9 <romashimanchik04@gmail.com>
2024-06-19 13:06:22 +0300

    ProductMapper

--- src/main/java/com/example/pizzapp/mappers/ProductMapper.java
+++ src/main/java/com/example/pizzapp/mappers/ProductMapper.java
@@ -1,6 +1,8 @@
 package com.example.pizzapp.mappers;
 
-import com.example.pizzapp.dto.ProductDto;
+import com.example.pizzapp.dto.request.create.ProductCreateRequest;
+import com.example.pizzapp.dto.request.update.ProductUpdateRequest;
+import com.example.pizzapp.dto.response.ProductResponse;
 import com.example.pizzapp.models.Product;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -11,9 +13,23 @@ import java.util.List;
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface ProductMapper {
     @Mapping(source = "product.category.id", target = "categoryId")
-    ProductDto toDto(Product product);
+    ProductCreateRequest toCreateRequest(Product product);
+    List<ProductCreateRequest> toCreateRequest(List<Product> products);
 
-    List<ProductDto> toDto(List<Product> products);
     @Mapping(target = "category", ignore = true)
-    Product toEntity(ProductDto productDto);
+    Product createRequestToEntity(ProductCreateRequest productCreateRequest);
+
+    @Mapping(source = "product.category.id", target = "categoryId")
+    ProductUpdateRequest toUpdateRequest(Product product);
+    List<ProductUpdateRequest> toUpdateRequest(List<Product> products);
+
+    @Mapping(target = "category", ignore = true)
+    Product updateRequestToEntity(ProductUpdateRequest productUpdateRequest);
+
+    @Mapping(source = "product.category.id", target = "categoryId")
+    ProductResponse toResponse(Product product);
+    List<ProductResponse> toResponse(List<Product> products);
+
+    @Mapping(target = "category", ignore = true)
+    Product responseToEntity(ProductResponse productResponse);
 }
