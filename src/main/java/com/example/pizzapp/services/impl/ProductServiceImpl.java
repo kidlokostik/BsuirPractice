a2d01e8cd76073637962d5cb417c1a1a30788cdf
Reknudt <pochta.navsegda@mail.ru>
2024-06-19 16:11:36 +0300

    product service

--- src/main/java/com/example/pizzapp/services/impl/ProductServiceImpl.java
+++ src/main/java/com/example/pizzapp/services/impl/ProductServiceImpl.java
@@ -1,28 +1,42 @@
 package com.example.pizzapp.services.impl;
 
-import com.example.pizzapp.dto.ProductDto;
+import com.example.pizzapp.dto.request.create.ProductCreateRequest;
+import com.example.pizzapp.dto.request.update.ProductUpdateRequest;
+import com.example.pizzapp.dto.response.ProductResponse;
+import com.example.pizzapp.exception.ResourceNotFoundException;
 import com.example.pizzapp.mappers.ProductMapper;
 import com.example.pizzapp.models.Product;
 import com.example.pizzapp.repositories.ProductRepository;
 import com.example.pizzapp.services.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService {
-
 
     private ProductRepository productRepository;
     private ProductMapper productMapper;
+
     @Override
-    public Product createProduct(Product product) {
-        return productRepository.createProduct(product);
+    public ProductResponse createProduct(ProductCreateRequest createProductRequest) {
+        Product product = productMapper.createRequestToEntity(createProductRequest);
+        return productMapper.toResponse(productRepository.save(product));
     }
 
     @Override
-    public Product updateProduct(Product product) {
-        return productRepository.updateProduct(product);
+    public ProductResponse updateProduct(Long id, ProductUpdateRequest updateProductRequest) {
+        Product newestProduct = findProductyByIdOrThrow(id);
+        productMapper.toUpdateRequest(newestProduct);
+
+        Product updateProduct = productRepository.save(newestProduct);
+        return productMapper.toResponse(updateProduct);
+    }
+
+    private Product findProductyByIdOrThrow(Long id) {
+        return productRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(""));
     }
 
     @Override
@@ -31,14 +45,15 @@ public class ProductServiceImpl implements ProductService {
     }
 
     @Override
-    public Product getProductId(Long id) {
-        return productRepository.getById(id);
+    public ProductResponse getProductById(Long id) {
+        Product product = findProductyByIdOrThrow(id);
+        return productMapper.toResponse(product);
     }
 
     @Override
-    public List<ProductDto> getAllProducts() {
+    public List<ProductResponse> getAllProducts() {
         return productRepository.findAll().stream()
-                .map(productMapper::toDto)
+                .map(productMapper::toResponse)
                 .toList();
     }
 }
