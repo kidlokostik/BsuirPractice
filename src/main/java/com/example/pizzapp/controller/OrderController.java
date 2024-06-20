a1cf28e3be1101c59881a63674088137277f1131
kidlokostik <kidlokostic12@gmail.com>
2024-06-21 00:19:51 +0300

    added controllers

---
+++ src/main/java/com/example/pizzapp/controller/OrderController.java
@@ -0,0 +1,49 @@
+package com.example.pizzapp.controller;
+
+import com.example.pizzapp.dto.request.create.OrderCreateRequest;
+import com.example.pizzapp.dto.request.create.OrderItemCreateRequest;
+import com.example.pizzapp.dto.response.OrderResponse;
+import com.example.pizzapp.service.OrderService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/order")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    public OrderResponse createOrder(@RequestBody @Valid OrderCreateRequest orderCreateRequest) {
+        return orderService.createOrder(orderCreateRequest);
+    }
+
+    @GetMapping("/{id}")
+    public OrderResponse getById(@PathVariable Long id) {
+        return orderService.getOrderById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteOrder(@PathVariable Long id) {
+        orderService.deleteOrder(id);
+    }
+
+    @GetMapping
+    public List<OrderResponse> getAllOrders() {
+        return orderService.getAllOrders();
+    }
+
+    @PutMapping
+    public void addOrderItem(Long orderId, OrderItemCreateRequest orderItemCreateRequest) {
+        orderService.addOrderItem(orderId, orderItemCreateRequest);
+    }
+
+    @PutMapping("/{orderId}")
+    public void addOrderItems(Long orderId, List<OrderItemCreateRequest> orderItemCreateRequests) {
+        orderService.addOrderItems(orderId, orderItemCreateRequests);
+    }
+}
---
+++ src/main/java/com/example/pizzapp/controller/ProductController.java
@@ -0,0 +1,42 @@
+package com.example.pizzapp.controller;
+
+import com.example.pizzapp.dto.request.create.ProductCreateRequest;
+import com.example.pizzapp.dto.response.ProductResponse;
+import com.example.pizzapp.mapper.ProductMapper;
+import com.example.pizzapp.models.Product;
+import com.example.pizzapp.service.ProductService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("product")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    public ProductResponse createProduct(@RequestBody @Valid ProductCreateRequest productCreateRequest) {
+        return productService.createProduct(productCreateRequest);
+    }
+
+    @GetMapping("/{id}")
+    public ProductResponse getById(@PathVariable Long id) {
+        return productService.getProductById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+    }
+
+    @GetMapping
+    public List<ProductResponse> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+}
