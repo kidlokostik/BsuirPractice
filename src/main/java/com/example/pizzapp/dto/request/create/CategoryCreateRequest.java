b76faa020fe8be988db7ac16f7376df355b4d9be
AniKi9 <romashimanchik04@gmail.com>
2024-06-20 14:11:16 +0300

    Category

--- src/main/java/com/example/pizzapp/dto/request/create/CategoryCreateRequest.java
+++ src/main/java/com/example/pizzapp/dto/request/create/CategoryCreateRequest.java
@@ -4,8 +4,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record CategoryCreateRequest(
-        @NotNull(message = "{field.null}")
-        Long id,
         @NotBlank(message = "{field.required}")
         String name
 ){}
--- src/main/java/com/example/pizzapp/dto/request/update/CategoryUpdateRequest.java
+++ src/main/java/com/example/pizzapp/dto/request/update/CategoryUpdateRequest.java
@@ -4,8 +4,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record CategoryUpdateRequest(
-        @NotNull(message = "{field.null}")
-        Long id,
         @NotBlank(message = "{field.required}")
         String name
 ){}
