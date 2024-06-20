d67da2349fdffbe1ee1a9a90f1e87477fab18da2
AniKi9 <romashimanchik04@gmail.com>
2024-06-20 13:55:51 +0300

    Количество в OrderItemResponse

--- src/main/java/com/example/pizzapp/dto/response/OrderItemResponse.java
+++ src/main/java/com/example/pizzapp/dto/response/OrderItemResponse.java
@@ -3,5 +3,6 @@ package com.example.pizzapp.dto.response;
 public record OrderItemResponse(
         Long id,
         Long productId,
-        Long orderId
+        Long orderId,
+        Long quantity
 ){}
