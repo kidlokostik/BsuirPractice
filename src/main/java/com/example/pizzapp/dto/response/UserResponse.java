ba5b6aba56dcbb171caae2ec9959d878b7a50507
AniKi9 <romashimanchik04@gmail.com>
2024-06-20 14:17:02 +0300

    Список заказов в юзере

--- src/main/java/com/example/pizzapp/dto/response/UserResponse.java
+++ src/main/java/com/example/pizzapp/dto/response/UserResponse.java
@@ -1,9 +1,14 @@
 package com.example.pizzapp.dto.response;
 
+import com.example.pizzapp.models.Order;
+
+import java.util.ArrayList;
+
 public record UserResponse(
         Long id,
         String phone,
         String name,
         String password,
-        String confirmPassword
+        String confirmPassword,
+        ArrayList<Order> orders
 ){}
