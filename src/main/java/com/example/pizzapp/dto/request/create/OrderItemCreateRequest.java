aa4aa6f0dcd736e6ae14edca037d5127ae5f8e6b
AniKi9 <romashimanchik04@gmail.com>
2024-06-20 13:49:58 +0300

    OrderItemRequests

--- src/main/java/com/example/pizzapp/dto/request/create/OrderItemCreateRequest.java
+++ src/main/java/com/example/pizzapp/dto/request/create/OrderItemCreateRequest.java
@@ -1,12 +1,12 @@
 package com.example.pizzapp.dto.request.create;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 public record OrderItemCreateRequest(
         @NotNull(message = "{field.null}")
-        Long id,
-        @NotNull(message = "{field.null}")
         Long productId,
         @NotNull(message = "{field.null}")
-        Long orderId
+        @Positive(message = "{field.positive}")
+        Long quantity
 ){}
--- src/main/java/com/example/pizzapp/dto/request/update/OrderItemUpdateRequest.java
+++ src/main/java/com/example/pizzapp/dto/request/update/OrderItemUpdateRequest.java
@@ -1,12 +1,12 @@
 package com.example.pizzapp.dto.request.update;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 public record OrderItemUpdateRequest(
         @NotNull(message = "{field.null}")
-        Long id,
-        @NotNull(message = "{field.null}")
         Long productId,
         @NotNull(message = "{field.null}")
-        Long orderId
+        @Positive(message = "{field.positive}")
+        Long quantity
 ){}
