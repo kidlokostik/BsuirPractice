c7cf541230aa40d4a4ebfd07205562eb1b84c8e3
AniKi9 <romashimanchik04@gmail.com>
2024-06-20 14:01:36 +0300

    Список orderItem в OrderResponse

--- src/main/java/com/example/pizzapp/dto/response/OrderResponse.java
+++ src/main/java/com/example/pizzapp/dto/response/OrderResponse.java
@@ -1,10 +1,14 @@
 package com.example.pizzapp.dto.response;
 
+import com.example.pizzapp.models.OrderItem;
+
 import java.math.BigDecimal;
+import java.util.ArrayList;
 
 public record OrderResponse(
         Long id,
         Long userId,
         String address,
-        BigDecimal price
+        BigDecimal price,
+        ArrayList<OrderItem> orderItem
 ){}
