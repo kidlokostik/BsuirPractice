e0f6d9127731d824b54c627c0891370523d77fa6
AniKi9 <romashimanchik04@gmail.com>
2024-06-23 14:27:39 +0300

    зависимости jjwt и начало JwtTokenProvider

--- src/main/java/com/example/pizzapp/security/JwtTokenProvider.java
+++ src/main/java/com/example/pizzapp/security/JwtTokenProvider.java
@@ -1,8 +1,26 @@
 package com.example.pizzapp.security;
 
+import com.example.pizzapp.security.prop.JwtProperties;
+import com.example.pizzapp.service.UserService;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
+import java.security.Key;
+
 @Service
+@RequiredArgsConstructor
 public class JwtTokenProvider {
 
+    private final JwtProperties jwtProperties;
+    private final UserDetailsService userDetailsService;
+    private final UserService userService;
+    private Key key;
+
+    @PostConstruct
+    public void init(){
+        this.key = Keys.hmacShaKeyFor(jwtProperties.getSecret().getBytes());
+    }
 }
