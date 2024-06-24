fe13461b2426849be39288d8ed53905aec3d7eb2
AniKi9 <romashimanchik04@gmail.com>
2024-06-24 22:26:31 +0300

    createAccessToken

--- src/main/java/com/example/pizzapp/security/JwtTokenProvider.java
+++ src/main/java/com/example/pizzapp/security/JwtTokenProvider.java
@@ -2,6 +2,8 @@ package com.example.pizzapp.security;
 
 import com.example.pizzapp.security.prop.JwtProperties;
 import com.example.pizzapp.service.UserService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +11,10 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -24,4 +30,29 @@ public class JwtTokenProvider {
         this.key = Keys.hmacShaKeyFor(jwtProperties.getSecret().getBytes());
     }
 
+    public String createAccessToken(Long userId, String username, Set<Role> roles){
+        Claims claims = Jwts.claims()
+                .setSubject(username)
+                .build();
+        claims.put("id", userId);
+        claims.put("roles", resolveRoles(roles));
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + jwtProperties.getAccess());
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(key)
+                .compact();
+    }
+
+    private List<String> resolveRoles(Set<Role> roles){
+        return roles.stream()
+                .map(Enum::name)
+                .collect(Collectors.toList());
+    }
+
+    public enum Role{
+        ADMIN, CUSTOMER
+    }
 }
