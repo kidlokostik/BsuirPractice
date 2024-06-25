18c8338ef515f24267a05606643469604484b5ed
AniKi9 <romashimanchik04@gmail.com>
2024-06-25 22:38:41 +0300

    NotBlank

--- src/main/java/com/example/pizzapp/security/dto/JwtRequest.java
+++ src/main/java/com/example/pizzapp/security/dto/JwtRequest.java
@@ -1,14 +1,15 @@
 package com.example.pizzapp.security.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class JwtRequest {
 
-    @NotNull(message = "{field.required}")
+    @NotBlank(message = "{field.required}")
     String email;
 
-    @NotNull(message = "{field.required}")
+    @NotBlank(message = "{field.required}")
     String password;
 }
