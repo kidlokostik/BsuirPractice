dc6eff2887814d7058e5e37d415dc81c94d6a60b
AniKi9 <romashimanchik04@gmail.com>
2024-06-25 22:16:38 +0300

    JwtEntityFactory added

---
+++ src/main/java/com/example/pizzapp/security/JwtEntityFactory.java
@@ -0,0 +1,32 @@
+package com.example.pizzapp.security;
+
+import com.example.pizzapp.model.Role;
+import com.example.pizzapp.model.RoleType;
+import com.example.pizzapp.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class JwtEntityFactory {
+
+    public static JwtEntity create(User user){
+        return new JwtEntity(
+                user.getId(),
+                user.getLogin(),
+                user.getName(),
+                user.getPassword(),
+                mapToGrantedAuthorities(new ArrayList<>(user.getRoles()))
+        );
+    }
+
+    private static List<GrantedAuthority> mapToGrantedAuthorities(List<Role> roles){
+        return roles.stream()
+                .map(Role::getName)
+                .map(RoleType::name)
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+}
