bca502cf5917e99adc747b209ba75ffaf150207f
AniKi9 <romashimanchik04@gmail.com>
2024-06-26 16:21:16 +0300

    поле email добавлено

--- src/main/java/com/example/pizzapp/security/dto/JwtResponse.java
+++ src/main/java/com/example/pizzapp/security/dto/JwtResponse.java
@@ -5,6 +5,7 @@ import lombok.Data;
 @Data
 public class JwtResponse {
     private Long id;
+    private String email;
     private String login;
     private String accessToken;
     private String refreshToken;
