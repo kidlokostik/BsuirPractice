a9d5f34b11fada61894234b4f879a540f92b98b5
AniKi9 <romashimanchik04@gmail.com>
2024-06-27 14:30:08 +0300

    login переименован в username

--- src/main/java/com/example/pizzapp/security/dto/JwtResponse.java
+++ src/main/java/com/example/pizzapp/security/dto/JwtResponse.java
@@ -6,7 +6,7 @@ import lombok.Data;
 public class JwtResponse {
     private Long id;
     private String email;
-    private String login;
+    private String username;
     private String accessToken;
     private String refreshToken;
 }
