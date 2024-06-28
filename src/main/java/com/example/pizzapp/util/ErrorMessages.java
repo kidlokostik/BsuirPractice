ebc860dac8eafc29a9c265a76c0f4502accd5fe0
Reknudt <pochta.navsegda@mail.ru>
2024-06-28 13:42:16 +0300

    del string format method

--- src/main/java/com/example/pizzapp/util/ErrorMessages.java
+++ src/main/java/com/example/pizzapp/util/ErrorMessages.java
@@ -17,8 +17,4 @@ public final class ErrorMessages {
     public static final String PASSWORDS_DO_NOT_MATCH = "Passwords do not match";
   
     public static final String ALREADY_USED_MESSAGE = "Duplicate or already used value: %s";
-
-    public static String formatNotFoundMessage(String resourceName, Long id) {
-        return String.format(NOT_FOUND_MESSAGE, resourceName, id != null ? " with id = " + id : "");
-    }
 }
