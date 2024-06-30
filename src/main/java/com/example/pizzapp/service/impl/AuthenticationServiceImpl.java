f67c9e78d33d2acc586b53bddfda414befc4c25f
AniKi9 <romashimanchik04@gmail.com>
2024-06-30 18:17:27 +0300

    сверка пароля

--- src/main/java/com/example/pizzapp/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/example/pizzapp/service/impl/AuthenticationServiceImpl.java
@@ -11,7 +11,6 @@ import com.example.pizzapp.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
