ae7dd6ac6f69babdd9030b585ad8ee832116c84b
AniKi9 <romashimanchik04@gmail.com>
2024-07-01 21:00:52 +0300

    doFilter method added

--- src/main/java/com/example/pizzapp/security/JwtTokenFilter.java
+++ src/main/java/com/example/pizzapp/security/JwtTokenFilter.java
@@ -38,5 +38,6 @@ public class JwtTokenFilter extends GenericFilterBean {
                 }
             } catch (ResourceNotFoundException ignored) {}
         }
+        filterChain.doFilter(servletRequest, servletResponse);
     }
 }
