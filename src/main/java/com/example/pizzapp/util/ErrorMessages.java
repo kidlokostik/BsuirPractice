1fa6f051a6c14de376f23003122fc601610f7498
kidlokostik <kidlokostic12@gmail.com>
2024-07-02 14:28:57 +0300

    delete Duplicate

--- src/main/java/com/example/pizzapp/util/ErrorMessages.java
+++ src/main/java/com/example/pizzapp/util/ErrorMessages.java
@@ -6,8 +6,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class ErrorMessages {
 
-    public static final String DUPLICATE_FOUND_MESSAGE = "Duplicate in %s parameter. %s is already used.";
-
     public static final String VALIDATION_FAILED_MESSAGE = "Validation failed";
 
     public static final String HTTP_MESSAGE_NOT_READABLE_MESSAGE = "Request body is missing or could not be read";
@@ -21,8 +19,4 @@ public final class ErrorMessages {
     public static final String ALREADY_USED_MESSAGE = "Duplicate or already used value: %s";
 
     public static final String ACCESS_DENIED_MESSAGE = "Access denied";
-
-    public static String formatNotFoundMessage(String resourceName, Long id) {
-        return String.format(NOT_FOUND_MESSAGE, resourceName, id != null ? " with id = " + id : "");
-    }
 }
